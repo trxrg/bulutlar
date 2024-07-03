4168fbaac9e9d8cc096a286174851b563ca155e6
trxrg <tarikrg@gmail.com>
2024-07-03 17:52:02 +0300

    some styling

--- src/components/TagButton.jsx
+++ src/components/TagButton.jsx
@@ -1,7 +1,7 @@
 export default function TagButton({handleClick, cls, children}) {
     return (
-        <div className="inline-block rounded-lg mx-1 my-1 bg-green-200 p-0.5 shadow-lg">
-            <button type="button" onClick={handleClick} className= {cls + " text-green-800 bg-white px-2 py-1 rounded-lg"}>{children}</button>  
+        <div className="inline-block bg-indigo-400 hover:bg-indigo-600 text-white font-bold py-1 px-2 m-1 rounded-full shadow-md transition duration-300 ease-in-out">
+            <button type="button" onClick={handleClick}>{children}</button>  
         </div>
     );
 }

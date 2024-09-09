67358b73a20991fdc9309e51b5ecfb6351f22ee0
trxrg <tarikrg@gmail.com>
2024-09-09 16:33:25 +0300

    article model updated

--- backend/sequelize/model/article.model.js
+++ backend/sequelize/model/article.model.js
@@ -3,11 +3,19 @@ module.exports = (sequelize, DataTypes) => {
         title: { allowNull: false, type: DataTypes.STRING },
         date: { allowNull: false, type: DataTypes.DATE },
         number: { allowNull: false, type: DataTypes.INTEGER },
+        order: { type: DataTypes.INTEGER },
         explanation: { allowNull: false, type: DataTypes.TEXT },
-        explanationJson: { allowNull: false, type: DataTypes.JSON },
+        explanationJson: { type: DataTypes.JSON },
         text: { allowNull: false, type: DataTypes.TEXT },
-        textJson: { allowNull: true, type: DataTypes.JSON },
-        code: {allowNull: false, type: DataTypes.STRING }
+        textJson: { type: DataTypes.JSON },
+        code: { allowNull: false, type: DataTypes.STRING }
+    }, {
+        indexes: [
+            {
+                unique: true,
+                fields: ['order'],
+            },
+        ],
     });
 
     return Article;

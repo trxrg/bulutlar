751575e4807ce62617fb0d30dc83b267fd1fe941
trxrg <tarikrg@gmail.com>
2024-09-15 16:07:38 +0300

    readMainPanel layout fix

--- src/containers/read/ReadBody.jsx
+++ src/containers/read/ReadBody.jsx
@@ -107,7 +107,7 @@ const ReadBody = () => {
     }
 
     return (
-        <div className={`overflow-auto h-full px-6 py-2 ${fontSize}`}>
+        <div className={`overflow-auto h-full px-6 pb-6 ${fontSize}`}>
             <div onClick={() => setActiveEditorRef(explanationEditorRef)} className='border border-gray-300 rounded-lg shadow-lg p-4'>
                 <RichEditor name={'explanation'} htmlContent={article.explanation} rawContent={article.explanationJson} handleContentChange={updateExplanation} editable={editable} ref={explanationEditorRef}></RichEditor>
             </div>
--- src/containers/read/ReadMainPanel.jsx
+++ src/containers/read/ReadMainPanel.jsx
@@ -1,31 +1,17 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 
-import { ReadContext } from '../../store/read-context';
 import ReadHeader from './ReadHeader.jsx';
 import ReadBody from './ReadBody.jsx';
 
 const ReadMainPanel = () => {
 
-  const { article } = useContext(ReadContext);
-
-  const [showCode, setShowCode] = useState(false);
-
-
-  const toggleShowCode = () => {
-    setShowCode(prev => !prev);
-  }
-
   return (
-    <div className="h-full mx-auto bg-stone-50">
-      <div className="p-6 h-[25%] border-b shadow-lg">
+    <div className="h-full flex flex-col mx-auto bg-stone-50">
+      <div className="p-6 flex-shrink-0 border-b shadow-lg">
         <ReadHeader></ReadHeader>
       </div>
-      <div className="h-[70%]">
+      <div className="flex-1 overflow-y-auto">
         <ReadBody></ReadBody>
-      </div>
-      <div className='flex h-[5%]'>
-        <h2 className='mx-2 cursor-pointer hover:text-green-500' onClick={(toggleShowCode)}>{showCode ? 'Hide' : 'Show'} Code</h2>
-        {showCode && <h2>{article.code}</h2>}
       </div>
     </div>
   );

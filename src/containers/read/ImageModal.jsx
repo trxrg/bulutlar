b20fa79e4152735b669c005b263c5e5aea39054f
trxrg <tarikrg@gmail.com>
2024-10-02 11:45:43 +0300

    imageModal refactoring

--- src/containers/read/ImageModal.jsx
+++ src/containers/read/ImageModal.jsx
@@ -1,72 +1,25 @@
-// src/components/ImageModal.js
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Modal from 'react-modal';
-import { MagnifyingGlassPlusIcon, MagnifyingGlassMinusIcon, XMarkIcon } from '@heroicons/react/24/solid'; // Import v2 icons
+import { MagnifyingGlassPlusIcon, MagnifyingGlassMinusIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import ActionButton from '../../components/ActionButton';
 import { deleteImage } from '../../backend-adapter/BackendAdapter.js';
 import { ReadContext } from "../../store/read-context";
 
-Modal.setAppElement('#root'); // For accessibility reasons
+Modal.setAppElement('#root');
 
 const ImageModal = ({ isOpen, onClose, image }) => {
 
     const { syncArticleFromBE } = useContext(ReadContext);
-
     const [scale, setScale] = useState(1);
-    // const [initialScale, setInitialScale] = useState(1);
-    const imageRef = useRef();
 
     useEffect(() => {
         if (isOpen) {
             setScale(1);
-            // setInitialScale(1);
         }
     }, [isOpen]);
 
-    // useEffect(() => {
-    //     console.log('in useEffect');
-    //     if (imageRef.current) {
-    //         // Get the dimensions of the image and modal container
-    //         const img = imageRef.current;
-    //         const modal = img.parentElement.parentElement;
-
-    //         // Calculate the scale to fit the image within the modal
-    //         const imgWidth = img.naturalWidth;
-    //         const imgHeight = img.naturalHeight;
-    //         const modalWidth = modal.clientWidth;
-    //         const modalHeight = modal.clientHeight;
-
-    //         const widthScale = modalWidth / imgWidth;
-    //         const heightScale = modalHeight / imgHeight;
-    //         const newScale = Math.min(widthScale, heightScale);
-
-    //         console.log('modalHeight')
-    //         console.log(modalHeight)
-
-    //         console.log('modalWidth')
-    //         console.log(modalWidth)
-
-    //         console.log('imgWidth')
-    //         console.log(imgWidth)
-
-    //         console.log('imgHeight')
-    //         console.log(imgHeight)
-            
-    //         console.log('widthScale')
-    //         console.log(widthScale)
-
-    //         console.log('heightScale')
-    //         console.log(heightScale)
-
-
-
-    //         setInitialScale(newScale);
-    //         setScale(newScale);
-    //     }
-    // }, [imageRef, isOpen]);
-
-    const zoomIn = () => setScale(prevScale => Math.min(prevScale * 1.2, 10)); // Cap zoom scale
-    const zoomOut = () => setScale(prevScale => Math.max(prevScale / 1.2, 0.1)); // Cap zoom scale
+    const zoomIn = () => setScale(prevScale => Math.min(prevScale * 1.2, 10));
+    const zoomOut = () => setScale(prevScale => Math.max(prevScale / 1.2, 1));
 
     const handleDeleteImage = async () => {
         await deleteImage(image.id);
@@ -81,14 +34,11 @@ const ImageModal = ({ isOpen, onClose, image }) => {
             className="fixed inset-0 flex items-center justify-center p-4"
             overlayClassName="fixed inset-0 bg-black bg-opacity-75"
         >
-            {/* Modal Content */}
             <div className="relative rounded-lg shadow-lg">
-                {/* Image Container */}
                 <div className="max-w-[80vw] max-h-[80vh] overflow-auto">
                     {image && <img
                         src={image.data}
-                        alt="Zoomable"
-                        ref={imageRef}
+                        alt={image.name}
                         style={{
                             transform: `scale(${scale})`,
                             transformOrigin: 'left top',
@@ -101,8 +51,6 @@ const ImageModal = ({ isOpen, onClose, image }) => {
                     />}
                 </div>
 
-
-                {/* Buttons */}
                 <div className="fixed top-4 right-4 z-30 flex flex-col h-[80vh] justify-between items-center">
                     <div className='space-y-2'>
                         <button

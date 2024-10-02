a7b1d8cd13844d241524fea5e6f672a8260b7f95
trxrg <tarikrg@gmail.com>
2024-10-02 10:15:03 +0300

    rich editor edit styling

--- src/containers/read/RichEditor.jsx
+++ src/containers/read/RichEditor.jsx
@@ -163,7 +163,7 @@ const RichEditor = React.forwardRef(({ name, htmlContent, rawContent, handleCont
 
     return (
         <div className="mx-auto">
-            <div onClick={handleEditorClick} className={(editable ? '' : 'caret-transparent ') + 'relative'}>
+            <div onClick={handleEditorClick} className={(editable ? 'border-2 border-stone-300 bg-white ' : 'caret-transparent ') + 'relative'}>
                 {showContextMenu && (
                     <div className="context-menu" style={{ top: contextMenuPosition.y, left: contextMenuPosition.x }}>
                         <button onClick={handleRemoveLink} className='hover:bg-red-300'>Remove Link</button>

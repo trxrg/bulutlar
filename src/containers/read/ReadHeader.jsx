2ab7b3c5ef7b2283f87d1611242ac198ddc97a29
trxrg <tarikrg@gmail.com>
2024-10-10 18:48:11 +0300

    readHeader owner problem fixed

--- src/containers/read/ReadHeader.jsx
+++ src/containers/read/ReadHeader.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 
 import { ReadContext } from '../../store/read-context.jsx';
+import { DBContext } from '../../store/db-context.jsx';
 import ReadControls from './ReadControls.jsx';
 
 const ReadHeader = () => {
   const { article } = useContext(ReadContext);
+  const { getOwnerById } = useContext(DBContext);
 
   return (
     <div className='overflow-auto'>
       <h2 className="text-3xl font-semibold text-gray-800">{article.title}</h2>
-      <p className="text-sm text-gray-600 mt-2">{article.owner && article.owner.name + " | "} {new Date(article.date).toLocaleDateString('tr')} ({article.number})</p>
+      <p className="text-sm text-gray-600 mt-2">{getOwnerById(article.ownerId).name + " | "} {new Date(article.date).toLocaleDateString('tr')} ({article.number})</p>
       <ReadControls></ReadControls>
     </div>
   );

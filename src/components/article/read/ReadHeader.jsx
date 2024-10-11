b042a51f457630033e945f038015051ae3caad93
trxrg <tarikrg@gmail.com>
2024-10-12 00:46:44 +0300

    readcontrols removed from readheader

--- src/components/article/read/ReadHeader.jsx
+++ src/components/article/read/ReadHeader.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 
 import { ReadContext } from '../../../store/read-context.jsx';
 import { DBContext } from '../../../store/db-context.jsx';
-import ReadControls from './ReadControls.jsx';
 
 const ReadHeader = () => {
   const { article } = useContext(ReadContext);
@@ -11,8 +10,7 @@ const ReadHeader = () => {
   return (
     <div className='overflow-auto'>
       <h2 className="text-3xl font-semibold text-gray-800">{article.title}</h2>
-      <p className="text-sm text-gray-600 mt-2">{getOwnerById(article.ownerId).name + " | " + getCategoryById(article.categoryId).name + " | "} {new Date(article.date).toLocaleDateString('tr')} ({article.number})</p>
-      <ReadControls></ReadControls>
+      <p className="text-md text-gray-600 mt-2">{getOwnerById(article.ownerId).name + " | " + getCategoryById(article.categoryId).name + " | "} {new Date(article.date).toLocaleDateString('tr')} ({article.number})</p>
     </div>
   );
 }

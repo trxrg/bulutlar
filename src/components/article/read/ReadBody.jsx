db59ec1bf03eb7f9ef033d6ac5bc1d35629c2db6
trxrg <tarikrg@gmail.com>
2024-10-13 00:15:20 +0300

    read body refactoring

--- src/components/article/read/ReadBody.jsx
+++ src/components/article/read/ReadBody.jsx
@@ -30,14 +30,6 @@ const ReadBody = () => {
         minSize={containerWidth * 0.6}
         maxSize={containerWidth * 0.8}
         size={rightPanelCollapsed ? '100%' : containerWidth * 0.8}
-        style={{
-          position: 'absolute',
-          left: 0,
-          overflow: 'hidden',
-          height: '100%',
-          transition: 'all 0.3s ease'
-        }}
-        paneStyle={{}}
         resizerStyle={rightPanelCollapsed ? { display: 'none' } : { background: '#6b6969', cursor: 'col-resize', width: '12px' }}
       >
         <SplitPane
@@ -45,17 +37,9 @@ const ReadBody = () => {
           minSize={containerWidth * 0.1}
           maxSize={containerWidth * 0.3}
           size={leftPanelCollapsed ? '0%' : containerWidth * 0.3}
-          style={{
-            position: 'absolute',
-            left: 0,
-            overflow: 'hidden',
-            height: '100%',
-            transition: 'all 0.3s ease'
-          }}
-          paneStyle={{}}
           resizerStyle={leftPanelCollapsed ? { display: 'none' } : { background: '#6b6969', cursor: 'col-resize', width: '12px' }}
         >
-          <div className={`h-full bg-gray-200 transition-transform duration-300 ${leftPanelCollapsed ? 'transform -translate-x-full w-0' : 'w-full'}`}>
+          <div className={`h-full transition-transform duration-300 ${leftPanelCollapsed ? 'transform -translate-x-full w-0' : 'w-full'}`}>
             {/* <BodyWithFixedHeader >
               <ReadControls />
               <ReadContent />
@@ -68,7 +52,7 @@ const ReadBody = () => {
             </BodyWithFixedHeader>
           </div>
         </SplitPane>
-        <div className={`h-full bg-gray-200 transition-transform duration-300 ${rightPanelCollapsed ? 'transform translate-x-full w-0' : 'w-full'}`}>
+        <div className={`h-full transition-transform duration-300 ${rightPanelCollapsed ? 'transform translate-x-full w-0' : 'w-full'}`}>
           {/* <ReadSidePanel /> */}
         </div>
       </SplitPane>

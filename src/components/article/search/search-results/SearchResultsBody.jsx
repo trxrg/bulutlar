b06f3c64e18f9755c161f4f28ce7aa1542b61b85
trxrg <tarikrg@gmail.com>
2024-11-20 14:37:49 +0300

    article adding instruction message added for first article

--- src/components/article/search/search-results/SearchResultsBody.jsx
+++ src/components/article/search/search-results/SearchResultsBody.jsx
@@ -6,7 +6,7 @@ import { DBContext } from '../../../../store/db-context.jsx';
 import { SearchContext } from '../../../../store/search-context.jsx';
 
 const SearchResultsBody = () => {
-    const { handleAddTab } = useContext(AppContext);
+    const { handleAddTab, translate: t } = useContext(AppContext);
     const { allArticles, getOwnerById, getTagById, getCategoryById } = useContext(DBContext);
     const { filtering, filteredArticles, setFilteredArticles } = useContext(SearchContext);
 
@@ -20,7 +20,7 @@ const SearchResultsBody = () => {
 
     const applyFiltering = (allArticles, filtering) => {
         let localFilteredArticles = allArticles;
-        
+
         // alternative normalize function
         // const normalizeText = (text) => {
         //     return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(/ı/g, 'i');
@@ -29,9 +29,9 @@ const SearchResultsBody = () => {
         const normalizeText = (text) => {
             if (!text)
                 return '';
-            const turkishMap = {'ç': 'c', 'ğ': 'g', 'ı': 'i', 'İ': 'I', 'ö': 'o', 'ş': 's', 'ü': 'u',};
+            const turkishMap = { 'ç': 'c', 'ğ': 'g', 'ı': 'i', 'İ': 'I', 'ö': 'o', 'ş': 's', 'ü': 'u', };
             return text.toLowerCase().split('').map(char => turkishMap[char] || char).join('');
-        };       
+        };
 
         if (filtering.ownerNames && filtering.ownerNames.length)
             localFilteredArticles = localFilteredArticles.filter(art => art.ownerId && filtering.ownerNames.includes(getOwnerById(art.ownerId).name));
@@ -45,10 +45,10 @@ const SearchResultsBody = () => {
         if (filtering.keywords && filtering.keywords.length) {
             localFilteredArticles = localFilteredArticles.filter(art => filtering.keywords.some(keyword => {
                 const normalizedKeyword = normalizeText(keyword);
-                return normalizeText(art.title).includes(normalizedKeyword) || 
-                       normalizeText(art.text).includes(normalizedKeyword) ||
-                       normalizeText(art.explanation).includes(normalizedKeyword) ||
-                       (art.comments[0] && normalizeText(art.comments[0].text).includes(normalizedKeyword));
+                return normalizeText(art.title).includes(normalizedKeyword) ||
+                    normalizeText(art.text).includes(normalizedKeyword) ||
+                    normalizeText(art.explanation).includes(normalizedKeyword) ||
+                    (art.comments[0] && normalizeText(art.comments[0].text).includes(normalizedKeyword));
             }));
         }
 
@@ -56,17 +56,23 @@ const SearchResultsBody = () => {
     };
 
     return (
-        <div className='flex flex-col gap-5 p-5 relative'>
-            {filteredArticles.map(art => (
-                <ArticleShort 
-                    handleClick={handleAddTab} 
-                    key={art.id} 
-                    article={art} 
-                    keywords={(filtering.keywords && filtering.keywords.length) ? filtering.keywords : null}
-                    dangerouslySetInnerHTML={{ __html: art.title }}
-                />
-            ))}
-        </div>
+        <>
+            {allArticles.length === 0 ?
+                <div className='flex justify-center p-2 h-full'>
+                    <p>{t('no articles')}</p>
+                </div> :
+                <div className='flex flex-col gap-5 p-5 relative'>
+                    {filteredArticles.map(art => (
+                        <ArticleShort
+                            handleClick={handleAddTab}
+                            key={art.id}
+                            article={art}
+                            keywords={(filtering.keywords && filtering.keywords.length) ? filtering.keywords : null}
+                            dangerouslySetInnerHTML={{ __html: art.title }}
+                        />
+                    ))}
+                </div>}
+        </>
     );
 };
 

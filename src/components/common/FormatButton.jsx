f29bed67484958cd6ccf62b712f849e3ab723dd4
trxrg <tarikrg@gmail.com>
2024-12-04 00:40:21 +0300

    formatButton styling

--- src/components/common/FormatButton.jsx
+++ src/components/common/FormatButton.jsx
@@ -2,7 +2,7 @@ const FormatButton = ({ onClick, onMouseDown, wfixed = true, children, ...props 
     return (
         <div {...props}>
             <button
-                className={(wfixed ? "w-10 " : "px-2 ") + "flex items-center justify-center h-10 bg-stone-200 hover:bg-stone-10 text-stone-400 hover:text-stone-700 rounded-md shadow-sm select-none"}
+                className={(wfixed ? "w-10 " : "px-2 ") + "flex items-center justify-center h-10 bg-stone-200 hover:bg-stone-100 text-stone-400 hover:text-stone-700 rounded-md shadow-sm select-none"}
                 onMouseDown={onMouseDown}
                 onClick={onClick}
             >

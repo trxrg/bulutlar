4b855ebec0767b8aaeee3e352f5599a6558e1314
trxrg <tarikrg@gmail.com>
2025-02-07 18:16:24 +0300

    link to tags screen added to tags panel in read screen

--- src/components/article/read/right-panel/TagsPanel.jsx
+++ src/components/article/read/right-panel/TagsPanel.jsx
@@ -4,12 +4,17 @@ import BodyWithFixedHeader from '../../../common/BodyWithFixedHeader';
 import { AppContext } from '../../../../store/app-context';
 
 const TagsPanel = () => {
-    const { translate: t } = useContext(AppContext);
+    const { translate: t, setActiveScreen } = useContext(AppContext);
+
+    const handleTitleClick = () => {
+        setActiveScreen('tags');
+    }
+
     return (
         <div className='bg-white my-2'>
             <BodyWithFixedHeader>
                 <div className='flex flex-wrap justify-between p-2 shadow-lg'>
-                    <h2 className='ml-2 text-xl font-semibold text-gray-800'>{t('tags')}</h2>
+                    <h2 className='ml-2 text-xl font-semibold text-gray-800 cursor-pointer hover:underline' onClick={handleTitleClick}>{t('tags')}</h2>
                 </div>
                 <div className='flex mt-3'>
                     <TagList2 />

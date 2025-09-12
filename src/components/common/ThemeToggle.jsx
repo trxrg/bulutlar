5a14f07375fbbd5424b814ba3541d84f7e5cd7b9
trxrg <tarikrg@gmail.com>
2025-09-12 20:12:20 +0300

    add translation to theme toggle button

--- src/components/common/ThemeToggle.jsx
+++ src/components/common/ThemeToggle.jsx
@@ -1,14 +1,17 @@
 import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
+import { useContext } from 'react';
+import { AppContext } from '../../store/app-context';
 import { useTheme } from '../../contexts/ThemeContext';
 import FormatButton from '../common/FormatButton';
 
 const ThemeToggle = () => {
+  const { translate: t } = useContext(AppContext);
   const { isDark, toggleTheme } = useTheme();
 
   return (
     <FormatButton 
       onClick={toggleTheme} 
-      title={isDark ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      title={isDark ? t('switch to light mode') : t('switch to dark mode')}
       className="theme-toggle-button"
     >
       {isDark ? (

ec7fdf0cad11f4233e0463b01ffa0918d1dc1cac
trxrg <tarikrg@gmail.com>
2025-09-17 10:52:36 +0300

    some styling for action button

--- src/components/common/ActionButton.jsx
+++ src/components/common/ActionButton.jsx
@@ -27,19 +27,19 @@ const ActionButton = ({ onClick, color='', size='', title='', children }) => {
         };
     }
 
-    let sizeStyle = "text-lg px-2 py-1";
+    let sizeStyle = "text-lg px-3 py-1 h-10";
 
     if (size === 'small')
-        sizeStyle = "text-sm px-2 py-1";
+        sizeStyle = "text-sm px-2 py-1 h-8";
     else if (size === 'large')
-        sizeStyle = "text-3xl px-5 py-3";
+        sizeStyle = "text-2xl px-5 py-3 h-12";
 
     return (
         <button
             className={"rounded-md shadow-sm select-none action-button " + sizeStyle}
             style={{
                 ...colorStyle,
-                fontWeight: '600'
+                fontWeight: '500'
             }}
             onClick={onClick}
             title={title || children}

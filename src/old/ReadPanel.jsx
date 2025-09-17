a7daeb92f2e333172f5f1f8702dfaa5829da62ac
trxrg <tarikrg@gmail.com>
2025-09-17 03:18:43 +0300

    remove old files

--- src/old/ReadPanel.jsx
+++
@@ -1,9 +0,0 @@
-import ArticleRead from "./ArticleRead";
-
-export default function ReadPanel({ article }) {
-    return (
-        <div>
-            <ArticleRead article={article}/>
-        </div>
-    );
-}

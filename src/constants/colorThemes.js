44561520e37954576cf825bc21f85c53c84f1aa0
trxrg <tarikrg@gmail.com>
2026-06-12 15:04:21 +0300

    add more color themes

--- src/constants/colorThemes.js
+++ src/constants/colorThemes.js
@@ -4,6 +4,16 @@ export const COLOR_THEMES = [
   { id: 'pink', swatch: '#AE6B8C' },
   { id: 'purple', swatch: '#8C6BAE' },
   { id: 'amber', swatch: '#AE8C6B' },
+  { id: 'coral', swatch: '#AE7B6B' },
+  { id: 'teal', swatch: '#6BAE9E' },
+  { id: 'lavender', swatch: '#9B8CAE' },
+  { id: 'rose', swatch: '#AE8B95' },
+  { id: 'sage', swatch: '#7A9475' },
+  { id: 'sky', swatch: '#6B9BAE' },
+  { id: 'peach', swatch: '#AE9B8B' },
+  { id: 'lilac', swatch: '#A68CAE' },
+  { id: 'mint', swatch: '#6BAE8C' },
+  { id: 'slate', swatch: '#7A8A9B' },
 ];
 
 export const DEFAULT_COLOR_THEME = 'green';
